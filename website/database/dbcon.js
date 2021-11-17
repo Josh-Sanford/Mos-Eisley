a79fdc61f87ac8da99f143d71bac380676e3cb72
Erik-Handeland <erikhandeland@gmail.com>
2021-11-17 12:52:31 -0800

    Delete dbcon.js

--- website/database/dbcon.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-var pool = mysql.createPool({
-  connectionLimit : 10,
-  host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_handelae',
-  password        : 'tythub-pYkquw-3gibvu',
-  database        : 'cs340_handelae'
-});
-module.exports.pool = pool;
